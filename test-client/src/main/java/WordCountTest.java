cab0251f47e925519d101bbda4a142953a3fa1c4
justinagain <jmdv@buffalo.edu>
2012-11-18 13:31:43 -0500

    Added driver for Queue class

--- test-client/src/main/java/WordCountTest.java
+++ test-client/src/main/java/WordCountTest.java
@@ -17,45 +17,68 @@ public class WordCountTest {
 
     public static void main(String[] args) throws IOException {
         WordCountTest wct = new WordCountTest();
-        wct.run();
+        wct.runNoQueue();
+        wct.runWithQueue();
     }
 
-    private void run() throws IOException {
+    public void runWithQueue() throws IOException {
+        File files[] = getWikipediaFiles();
+        Queue<Map<String, AtomicInteger>> wordCounts = compute(files);
+        int processed = 0;
+        Map<String, AtomicInteger> finalMap = new HashMap<String, AtomicInteger>();
+        System.out.println("Enter test to see what is finished on queue");
+        if(! wordCounts.isEmpty() && processed <= files.length / 2 ){
+                System.out.println("Merging a map in");
+                merge(finalMap, wordCounts.remove());
+        }
+        wordCounts.clear();
+        System.out.println("Completed processing at least half of the queue files.  Finished with size: " + finalMap.size());
+    }
+
+    //@FuturableQueue
+    private Queue compute(File[] files) throws IOException {
         ApplicationContext context = new ClassPathXmlApplicationContext("springContext.xml");
-
         WordCountGenerator wordCountGenerator = context.getBean("wordCountGenerator", WordCountGenerator.class);
+        Queue<Map<String, AtomicInteger>> wordCounts = new LinkedList<Map<String, AtomicInteger>>();
+        for(File file: files){
+            wordCounts.add(wordCountGenerator.generateWordCount(file));
+            System.out.println("Sent one out ... " + file.getName());
+        }
+        return wordCounts;
+    }
 
-        Resource resourceOne = new ClassPathResource(ALL_WIKIPEDIA_FILE_LOCATION);
-        resourceOne.getFile().listFiles();
-        File[] files = resourceOne.getFile().listFiles();
-        //new String[]{"englishText_30000_40000", "englishText_40000_50000", "englishText_50000_60000", "englishText_60000_70000"};
+
+    public void runNoQueue() throws IOException {
+        File files[] = getWikipediaFiles();
+        run(files);
+    }
+
+    private void run(File[] files) throws IOException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("springContext.xml");
+        WordCountGenerator wordCountGenerator = context.getBean("wordCountGenerator", WordCountGenerator.class);
         Queue<Map<String, AtomicInteger>> wordCounts = new LinkedList<Map<String, AtomicInteger>>();
 
         for(File file: files){
-
-//            Resource resource = new ClassPathResource(ALL_WIKIPEDIA_FILE_LOCATION + file.getName());
-
             wordCounts.add(wordCountGenerator.generateWordCount(file));
             System.out.println("Sent one out ... " + file.getName());
         }
         System.out.println("Finished sending out all");
-
-//        for (Map<String, AtomicInteger> wordCount : wordCounts) {
-//            System.out.println("Values: " + wordCount.size());
-//        }
-
         Map<String, AtomicInteger> finalMap = new HashMap<String, AtomicInteger>();
 
         while(!wordCounts.isEmpty()) {
             System.out.println("Merging a map in");
             merge(finalMap, wordCounts.remove());
         }
-
         System.out.println("Finished all!! Final map size :" + finalMap.size());
         ((ConfigurableApplicationContext) context).close();
 
     }
 
+    public File[] getWikipediaFiles() throws IOException {
+        Resource resourceOne = new ClassPathResource(ALL_WIKIPEDIA_FILE_LOCATION);
+        resourceOne.getFile().listFiles();
+        return resourceOne.getFile().listFiles();
+    }
 
     private void merge(Map<String, AtomicInteger> a, Map<String, AtomicInteger> b) {
         for (Map.Entry<String, AtomicInteger> entry : b.entrySet()) {
