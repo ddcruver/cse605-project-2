273a8d1e505a73964da14beb05e03226af464d53
Dan Cruver <ddcruver@gmail.com>
2012-11-24 23:22:58 -0500

    Commit some javadocs.

--- futurable-lib/src/main/java/edu/buffalo/cse/cse605/project2/PartialFuturableMarker.java
+++ futurable-lib/src/main/java/edu/buffalo/cse/cse605/project2/PartialFuturableMarker.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Defines a method call that marks a hashCombination, coordinate pair, etc to be ready to be read by downstairs consumer.
+ * 
+ * This method is exactly like the {@link PartialFuturableSetter} except does not expect a value to be set just marked as clean.
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PartialFuturableMarker {
